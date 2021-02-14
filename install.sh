6b3b290feac930c4e3a1e6cf5f77c4a3e39bcb4c
Vivek Dubey <dubey.vivek.2805@gmail.com>
2021-02-14 13:12:46 +0530

    added support for local mongodb

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
 # install nvm 
-sudo apt-get install -y nvm
-sudo nvm install -y 14.7.0
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# activate nvm
+. ~/.nvm/nvm.sh
+
+nvm install 14.7.0
+
+# install mongoDB
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt update
+
+sudo apt install -y mongodb-org
+
+# start the service
+sudo systemctl start mongod
+
+sudo systemctl status mongod
+
+# enable the service
+sudo systemctl enable mongod
--- server_run.sh
+++ server_run.sh
@@ -6,4 +6,4 @@ cd backend
 
 npm install
 
-npm run
+npm start
